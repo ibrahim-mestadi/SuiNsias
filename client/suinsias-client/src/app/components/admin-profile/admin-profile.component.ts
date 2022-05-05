a80e00030fcb1601ef443dfd5e28e49484a8880c
ibrahim <ibrahim_mestadi@um5.ac.ma>
2022-05-05 22:03:21 +0000

    display user profile compo

--- client/suinsias-client/src/app/components/admin-profile/admin-profile.component.ts
+++ client/suinsias-client/src/app/components/admin-profile/admin-profile.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-admin-profile',
   templateUrl: './admin-profile.component.html',
-  styleUrls: ['./admin-profile.component.css']
+  styleUrls: ['./admin-profile.component.scss']
 })
 export class AdminProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthenticationService,private router:Router) { }
 
-  ngOnInit(): void {
+
+  ngOnInit() {
   }
 
 }
--- client/suinsias-client/src/app/services/authentication.service.ts
+++ client/suinsias-client/src/app/services/authentication.service.ts
@@ -1,9 +1,143 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  public host = 'http://localhost:8085';
+  public jwt: string;
+  private email: string;
+  public firstName: string;
+  public lastName: string;
+  public specialte: string ;
+  public birthday: string ;
+  public grade: any ; 
+  public specialite: any ;  
+  private roles: Array<string>;
+  public convertToUser_ = false;
+  public id: any;
+  constructor(private httpClient: HttpClient) {
+    try {
+      this.loadToken();
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
+  public login(data) {
+    return this.httpClient.post(this.host + '/login', data, { observe: 'response' });
+  }
+
+
+  public saveToken(jwt) {
+    localStorage.setItem('token', jwt);
+    localStorage.setItem('isconvert', String(this.convertToUser_));
+    this.jwt = jwt;
+    this.parseJWT();
+  }
+
+  parseJWT() {
+    let jwtHelper = new JwtHelperService();
+    let objJwt = jwtHelper.decodeToken(this.jwt);
+    this.email = objJwt.sub;
+    console.log(this.email);
+     
+    this.getUserByEmail(this.email).subscribe
+    (
+      data=>{
+          this.firstName=data["firstname"];
+          this.lastName=data["lastname"];
+          this.grade = data["grade"] ; 
+          this.specialte = data["specialite"] ;
+          this.id = data["id"] ;
+          console.log(data);
+      },
+      err=>{
+        console.log("error");
+      }
+    );
+    console.log(this.email);
+    this.roles = objJwt.roles;
+    if (this.convertToUser_) {
+      this.convertToUser();
+    }
+
+  }
+
+
+  convertToUser() {
+    const index = this.roles.indexOf('ADMIN', 0);
+    if (index > -1) {
+      this.roles.splice(index, 1);
+    }
+  }
+  onlyAdmin() {
+    if (!this.isAdmin()) {
+      this.logOut();
+      return false;
+    }
+    return true;
+  }
+
+  isAdmin() {
+    return this.roles.indexOf('ADMIN') >= 0;
+  }
+
+  isUser() {
+    return this.roles.indexOf('USER') >= 0;
+  }
+
+  isAuthentificated() {
+    return this.roles != undefined;
+  }
+
+  loadToken() {
+    this.jwt = localStorage.getItem('token');
+    this.convertToUser_ = (localStorage.getItem('isconvert') == 'true');
+    this.parseJWT();
+  }
+  logOut() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('isconvert');
+    this.initParams();
+  }
+
+  initParams() {
+    this.jwt = undefined;
+    this.email = undefined;
+    this.roles = undefined;
+  }
+
+  getAllUsers() {
+    let headers = new HttpHeaders({ 'authorization': 'Bearer' + this.jwt });
+    return this.httpClient.get(this.host + '/users', { headers: headers });
+  }
+  getUserByEmail(email) {
+    let headers = new HttpHeaders({ 'authorization': 'Bearer' + this.jwt });
+    return this.httpClient.get(this.host + '/users/search/byEmail?email='+email, { headers: headers });
+  }
+  getAllRoles() {
+    let headers = new HttpHeaders({ 'authorization': 'Bearer' + this.jwt });
+    return this.httpClient.get(this.host + '/appRoles', { headers: headers });
+  }
+
+  public getRessource(url) {
+    let headers = new HttpHeaders({ 'authorization': 'Bearer' + this.jwt });
+    return this.httpClient.get(url, { headers: headers });
+  }
+  public postRessource(url, data) {
+    let headers = new HttpHeaders({ 'authorization': 'Bearer' + this.jwt });
+    return this.httpClient.post(url, data, { headers: headers });
+  }
+
+  public deleteRessource(url) {
+    let headers = new HttpHeaders({ 'authorization': 'Bearer' + this.jwt });
+    return this.httpClient.delete(url, { headers: headers });
+  }
+
+
 }
