f26bb5ad921edd5bc17c5e3263bc60bac766671e
ibrahim <ibrahim_mestadi@um5.ac.ma>
2022-05-06 22:07:53 +0000

    navbar compo

--- client/suinsias-client/src/app/components/menubar/menubar.component.ts
+++ client/suinsias-client/src/app/components/menubar/menubar.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menubar',
   templateUrl: './menubar.component.html',
-  styleUrls: ['./menubar.component.css']
+  styleUrls: ['./menubar.component.scss']
 })
 export class MenubarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthenticationService,private router:Router) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+  OnDeconnexion() {
+    console.log("Deconnexion");
+    this.auth.logOut();
+    this.router.navigateByUrl('/index');
+
   }
 
 }
