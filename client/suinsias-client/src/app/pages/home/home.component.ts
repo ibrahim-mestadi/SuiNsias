fd99627567bbe7d875ad7c987a7f2966cd97b966
ibrahim <ibrahim_mestadi@um5.ac.ma>
2022-05-09 22:16:02 +0000

    contact

--- client/suinsias-client/src/app/pages/home/home.component.ts
+++ client/suinsias-client/src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { NavbarComponent } from './../../components/navbar/navbar.component';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  @ViewChild(NavbarComponent, { static: false }) navbar: NavbarComponent;
 
-  constructor() { }
+  constructor(private auth: AuthenticationService, private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+
+  isAuthentificated() {
+    return this.auth.isAuthentificated();
+  }
+  OnDeconnexion() {
+    console.log("Deconnexion");
+    this.auth.logOut();
+    this.router.navigateByUrl('/index');
   }
 
 }
