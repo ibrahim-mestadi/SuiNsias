c6c2c321088e78edef803ebbb478882013755ebe
ibrahim <ibrahim_mestadi@um5.ac.ma>
2022-05-09 22:15:19 +0000

    contact

--- client/suinsias-client/src/app/pages/contact/contact.component.ts
+++ client/suinsias-client/src/app/pages/contact/contact.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { AnnonceService } from './../../services/annonce.service';
+import { Component, OnInit, ContentChildren } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  errorMessage='';
+  message='';
+  constructor(private annonceService:AnnonceService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
   }
 
+  onSendMessage(value)
+  {
+    console.log(value);
+    const path = '/messages';
+    this.annonceService.postRessource(path, value).
+      subscribe(
+        data => {
+this.message='Votre message a été bien envoyé !';
+const source = timer(3000);
+const subscribe = source.subscribe(val => {this.message='';});
+        },
+        err => {
+          console.log(err);
+        }
+      );
+  }
 }
