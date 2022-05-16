f8dde38ec13dc6b3c9d003ee884397c9fb790e6b
= <=>
2022-05-16 02:16:30 +0100

    fixtckengithub

--- backend/api/src/main/java/com/code/api/api/entities/Message.java
+++ backend/api/src/main/java/com/code/api/api/entities/Message.java
@@ -3,6 +3,7 @@ package api.entities;
 import java.util.Date;
 
 import javax.persistence.Entity;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
