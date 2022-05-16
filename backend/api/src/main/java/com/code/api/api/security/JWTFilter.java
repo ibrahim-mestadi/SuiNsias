0f33ecc2f3b39e5ca5ff064577071221b73681e3
ibrahim <ibrahim_mestadi@um5.ac.ma>
2022-05-16 00:54:28 +0000

    secutity filters add

--- backend/api/src/main/java/com/code/api/api/security/JWTFilter.java
+++ backend/api/src/main/java/com/code/api/api/security/JWTFilter.java
@@ -1,32 +1,4 @@
-package doc.security;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import doc.service.AccountService;
-import doc.service.IAccountService;
 
 public class JWTFilter extends UsernamePasswordAuthenticationFilter {
 
